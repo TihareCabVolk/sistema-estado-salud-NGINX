358a0a902ff5a24400b2b4894fd8a8c50275cf00
TCab <tihare.cab@gmail.com>
2026-04-27 08:24:17 -0400

    fix:nginx.conf

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 IDreplica = os.environ.get('HOSTNAME','replica_desconocida')
-puerto_interno = int(os.environ.get('PUERTO', 5000))
+puerto_usado = int(os.environ.get('PUERTO', 5000))
 
 @app.route('/estado', methods=['GET'])
 
@@ -15,8 +15,8 @@ def estado():
         "cupos_restantes": 12,
         "mensaje": 'Atención disponible',
         "atendido_por_contenedor": IDreplica,
-        "puerto_interno": puerto_interno
+        "puerto_interno": puerto_usado
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=puerto_interno)
+    app.run(host='0.0.0.0', port=puerto_usado)
